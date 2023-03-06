843fbc404ae0dc5a9adc0cf65191797504d3bcac
C1329184 Fabio Freller <c1329184@interno.bb.com.br>
2023-03-06 13:31:40 -0300

    stop_after adicionado

--- move.py
+++ move.py
@@ -28,21 +28,33 @@ def random_moves_and_press(n_times, interval_seconds):
     
 
     
-def move_if_idle(max_minutes):
+def move_if_idle(max_minutes, stop_after):
     from time import sleep
     from win32api import GetCursorPos
+    from datetime import datetime
     print_with_time(f"Initializing...\n")
     max_seconds = int(max_minutes*60)
+    
+    if stop_after is not None:
+        stop_after_seconds = int(stop_after*60)
+        start_time = datetime.now()
+    
     while True:
         init_cursor_pos = GetCursorPos()
         sleep(max_seconds)
         cursor_pos = GetCursorPos()
+        if stop_after is not None:
+            now = datetime.now()
+            if (now-start_time).seconds >= stop_after_seconds:
+                print(f"Stopping after {stop_after} minutes")
+                return
         if cursor_pos == init_cursor_pos:
             random_moves_and_press(n_times=5, interval_seconds=.25)
             print_with_time(f"Moved from {cursor_pos} to {GetCursorPos()}")
         else:
             print_with_time(f"I won't interrupt. Pos: {cursor_pos}")
             continue
+
         
 
 if __name__ == "__main__":
@@ -50,12 +62,14 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="My parser")
     max_minutes_type = float
     parser.add_argument('--max_minutes', type=max_minutes_type)
+    parser.add_argument('--stop_after', type=max_minutes_type, default=None)
     args = parser.parse_args()
     max_minutes = args.max_minutes
+    stop_after = args.stop_after
     if type(max_minutes) != max_minutes_type:
         print("You need to pass the number of minutes")
     else:
-        move_if_idle(max_minutes)
+        move_if_idle(max_minutes, stop_after)
 
 
 
