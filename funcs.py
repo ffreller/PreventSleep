8d51c8beca7e818c6f5685ea8ab646918f414f9f
Fabio Freller <fabiofreller@Fabios-MacBook-Pro.local>
2023-09-10 20:20:48 -0300

    added docstring

--- funcs.py
+++ funcs.py
@@ -1,4 +1,11 @@
-def move_if_idle(check_interval, max_running_time):
+def move_if_idle(check_interval:  float, max_running_time: float):
+    """
+    Moves the cursor periodically and performs actions if the cursor remains idle.
+
+    Parameters:
+    - check_interval (float): Time in minutes between cursor position checks.
+    - max_running_time (float or None): Maximum time in minutes for the script to run. If None, the script runs indefinitely.
+    """
     from time import sleep
     from datetime import datetime
     from pyautogui import position
@@ -28,12 +35,24 @@ def move_if_idle(check_interval, max_running_time):
             continue
 
 
-def print_with_time(text):
+def print_with_time(text: str):
+    """
+    Prints a timestamp followed by the provided text.
+
+    Parameters:
+    - text (str): The text to be printed.
+    """
     from datetime import datetime
     print(f"{datetime.now().strftime('%H:%M:%S')} - {text}")
     
 
 def get_moves():
+    """
+    Generates random cursor positions within the screen size.
+
+    Returns:
+    - tuple: Two integers representing random cursor positions.
+    """
     from random import randint
     from pyautogui import size as screen_size
     max1, max2 = screen_size()
@@ -42,12 +61,26 @@ def get_moves():
 
 
 def random_move():
+    """
+    Moves the cursor to a random position on the screen.
+    """
     from pyautogui import moveTo
     pos1, pos2 = get_moves()
     moveTo(pos1, pos2)
 
 
-def random_moves_and_press(n_times, interval_seconds):
+def random_moves_and_press(n_times: int, interval_seconds: float):
+    """
+    Performs a sequence of actions:
+    1. Moves the cursor to a random position.
+    2. Right-clicks the mouse.
+    3. Presses the "esc" key.
+    4. Sleeps for a specified interval.
+
+    Parameters:
+    - n_times (int): Number of times to perform the sequence.
+    - interval_seconds (float): Time in seconds to sleep between sequences.
+    """
     from pyautogui import press, click
     from time import sleep
     for _ in range(n_times):
